9c84c11c442d1d9fc8617202e282439945f7289d
Dallin Lyman <142834133+Duna65@users.noreply.github.com>
2025-01-20 17:23:06 -0700

    Add files via upload

--- JavaPage/src.js
+++ JavaPage/src.js
@@ -1,3 +1,7 @@
 function ChangeColor(r,g,b){
 document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
 }
+function AddText(id,text){
+    const p = document.getElementById(id)
+p.innerText = text
+}
